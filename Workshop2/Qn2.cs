4a4339d82826629f5cd391f64c273fdfbd03b5b9
knghtedbiiscuit <alibiscuit@gmail.com>
2017-02-15 12:55:21 +0800

    Add project files.

---
+++ Workshop2/Qn2.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop2
+{
+    class Qn2
+    {
+        static void Main()
+        {
+            triangle t = new triangle();
+            Console.WriteLine(t.Area());
+            Console.WriteLine(t.Perimeter());
+        }
+    }
+
+
+    public class triangle
+    {
+        //attributes
+        double Base;
+        double Height;
+        double SideA;
+        double SideB;
+
+        //properties
+       public double Base2
+        {
+            get {
+                return Base2;
+            }
+
+            set
+            {
+                Base2 = value;
+            }
+        }
+
+        public double Height2
+        {
+            get
+            {
+                return Height2;
+            }
+
+            set
+            {
+                Height2 = value;
+            }
+        }
+
+        public double SideA2
+        {
+            get
+            {
+                return SideA2;
+            }
+
+            set
+            {
+                SideA2 = value;
+            }
+        }
+
+        public double SideB2
+        {
+            get
+            {
+                return SideB2;
+            }
+
+            set
+            {
+                SideB2 = value;
+            }
+        }
+
+        //constructor
+        public triangle() 
+        {
+            Console.WriteLine("Please enter Base length of triangle");
+            string input = Console.ReadLine();
+            Base = Convert.ToDouble(input);
+
+            Console.WriteLine("Please enter Height of triangle");
+            string input2 = Console.ReadLine();
+            Height = Convert.ToDouble(input2);
+
+            Console.WriteLine("Please enter 2nd side of triangle");
+            string input3 = Console.ReadLine();
+            SideA = Convert.ToDouble(input3);
+
+            Console.WriteLine("Please enter 3rd side of triangle");
+            string input4 = Console.ReadLine();
+            SideB = Convert.ToDouble(input4);
+        }
+
+        //methods
+        public double Area()
+        {
+            return (0.5 * Base * Height);
+        }
+
+        public double Perimeter()
+        {
+            return (Base + SideA + SideB);
+        }
+
+    }
+
+}
---
+++ Workshop2/Qn3.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ISS.RV.LIB;
+
+namespace Workshop2
+{
+    public class Dice
+    {
+        //attributes
+        int Faceup;
+        Random r = new Random();
+
+        //Properties
+        public string StrFaceUp()
+        {
+            string A = Convert.ToString(Faceup);
+            return A ;
+        }
+
+        public int GetFace()
+        {
+            return Faceup;
+        }
+
+        //Constructors
+        public Dice()
+        {
+            Throw();
+        }
+        
+        //Methods
+        public void Throw()
+        {
+            Faceup = r.Next(1, 7);
+        }
+        
+    }
+
+    class Qn3
+    {
+        static void Main()
+        {
+            int throwtimes = 10000000; // throwtimes refers to number of times a dice is thrown
+            Dice D1 = new Dice();
+            Dice D2 = new Dice();
+            int hit = 0;
+            double prob;
+
+            // throw two dices
+            for (int i = 0; i < throwtimes; i++)
+            {
+                D1.Throw();
+                D2.Throw();
+                if (D1.GetFace()+D2.GetFace() == 8)
+                {
+                    hit++;
+                }
+            }
+
+            // calculate prob of x throws and print
+            prob = Convert.ToDouble(hit) / Convert.ToDouble(throwtimes);
+            Console.WriteLine(prob);
+
+        }
+    }
+}
